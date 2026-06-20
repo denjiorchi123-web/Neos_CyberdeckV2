0724051a153df020d9987adc87541ccce51ea74c
MarksXman MarksXman <denjiorchi123@gmail.com>
2026-06-20 22:21:32 +0530

    Add WebKitGTK env variables for Tauri Wayland touch and GPU acceleration

--- deploy/scripts/start-kiosk.sh
+++ deploy/scripts/start-kiosk.sh
@@ -3,6 +3,10 @@ set -eu
 
 BASE_URL="http://127.0.0.1:3000"
 export XDG_SESSION_TYPE=wayland
+export GDK_BACKEND=wayland
+export WEBKIT_DISABLE_COMPOSITING_MODE=0
+export WEBKIT_FORCE_COMPOSITING_MODE=1
+export GDK_CORE_DEVICE_EVENTS=1
 
 # Wait for Next.js to start
 while ! curl -k -s -o /dev/null "$BASE_URL"; do
