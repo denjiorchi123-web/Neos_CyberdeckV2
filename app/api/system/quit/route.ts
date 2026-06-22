1760d77ec3be3710c5838c2b2bffd8dfe702915a
MarksXman MarksXman <denjiorchi123@gmail.com>
2026-06-23 00:40:50 +0530

    run killall with sudo in quit API

--- app/api/system/quit/route.ts
+++ app/api/system/quit/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-    // This forcefully kills all running instances of Chromium on the Linux host
-    exec("killall chromium");
+    // This forcefully kills all running instances of Chromium or Tauri on the Linux host
+    exec("sudo /usr/bin/killall cyberdeck 2>/dev/null || true; sudo /usr/bin/killall chromium 2>/dev/null || true");
     return NextResponse.json({ success: true });
   } catch (error) {
     console.error(error);
